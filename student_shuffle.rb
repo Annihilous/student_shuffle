b32bc9b357092e0fadb72420ba929502d64e3ba3
Annihilous <benjaminjnorton@gmail.com>
2015-01-29 09:38:31 -0500

    first commit

---
+++ student_shuffle.rb
@@ -0,0 +1,6 @@
+# Program splits the class into teams of two.
+# If there is an odd number of students, the last group will have three members.
+
+students = ["Alicia", "Anita", "Ben", "Cameron", "Daniel", "Jennifer", "Matthew", "Mark", "Thomas"]
+
+puts students
