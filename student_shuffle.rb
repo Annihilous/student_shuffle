e4427244b3c8321f7d5904f34a014fdef27b1644
Annihilous <benjaminjnorton@gmail.com>
2015-01-29 11:06:40 -0500

    final solution

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,6 +1,38 @@
 # Program splits the class into teams of two.
 # If there is an odd number of students, the last group will have three members.
 
-students = ["Alicia", "Anita", "Ben", "Cameron", "Daniel", "Jennifer", "Matthew", "Mark", "Thomas"]
+puts "Enter student names. Enter 'done' when finished."
 
-puts students.shuffle
+name = ""
+students = []
+
+# START INPUT
+while name != "Done"
+	name = gets.chomp.capitalize
+	students.push(name)
+end
+# END INPUT
+
+if students.length == 2
+	puts "#{students[0]} will be working alone today."
+else
+	students = students[0..-2].shuffle!
+	puts
+	puts "Your student groups are: "
+	count = 0
+		until count > students.length - 2	
+		if count < students.length - 3
+			puts "#{students[count]} & #{students[count + 1]}"
+		elsif students.length % 2 == 0
+			puts "#{students[count]} & #{students[count + 1]}"
+		else
+			puts "#{students[count]} & #{students[count + 1]} & #{students[(count + 2)]}"
+		end
+		count += 2
+	end
+end
+
+puts
+
+
+
